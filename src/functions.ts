6e708470b616d8a7b460f96579b44032453f0589
Logan Geiser <muclecake@gmail.com>
2023-09-12 15:52:05 -0400

    Added functions.ts

--- src/functions.ts
+++ src/functions.ts
@@ -30,8 +30,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    message.toUpperCase;
-    return message + "!";
+    const shoutString: string = message.toUpperCase();
+    return shoutString + "!";
 }
 
 /**
